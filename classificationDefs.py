bdf20e223e554339a9c37bdff981a7422937626a
Dario Malchiodi <malchiodi@di.unimi.it>
2013-01-08 20:30:38 +0100

    Added license and copyright statements

--- classificationDefs.py
+++ classificationDefs.py
@@ -1,3 +1,21 @@
+
+#*****************************************************************************
+#       Copyright (C) 2010 Dario Malchiodi <malchiodi@di.unimi.it>
+#
+# This file is part of svMathematica.
+# svMathematica is free software; you can redistribute it and/or modify it under the
+# terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation; either version 2.1 of the License, or (at your option)
+# any later version.
+# svMathematica is distributed in the hope that it will be useful, but without any
+# warranty; without even the implied warranty of merchantability or fitness
+# for a particular purpose. See the GNU Lesser General Public License for
+# more details.
+# You should have received a copy of the GNU Lesser General Public License
+# along with svMathematica; if not, see <http://www.gnu.org/licenses/>.
+#
+#*****************************************************************************
+
 from cvxopt import matrix,solvers
 from numpy import dot,array
 
